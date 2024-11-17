bedacdde64f7aa8d4578fe76f930a42ad1367d21
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2024-11-17 01:22:51 +0100

    Update main.rs
    Signed-off-by: Kristoffer Grundström <sailinggalaxians@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,96 @@
-use gtk::prelude::*;
-use gtk::{Button, Label, Orientation, Box};
+use eframe::egui;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 
-fn main() {
-    // Initialize GTK
-    gtk::init().expect("Failed to initialize GTK.");
+fn main() {}
+    const options = eframe::NativeOptions::default();
+match eframe::run_native(
+        "Sailfish OS HADK",
+        options,
+    Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+) {}
+    Ok(_) => println!("Application ran successfully!"),
+    Err(e) => eprintln!("Failed to run application: {}", e),
+{}
 
-    // Create main window
-    let window = gtk::Window::new(gtk::WindowType::Toplevel);
-    window.set_title("Welcome to the Sailfish OS HADK GUI");
-    window.set_default_size(300, 200);
+struct MyApp {}
+    android_root: String,
+    vendor: String,
+    device: String,
+    port_arch: String,
+{}
 
-    // Create a box to add widgets vertically
-    let vbox = Box::new(Orientation::Vertical, 5);
-    window.add(&vbox);
+impl Default for MyApp {}
+    fn default() -> Self {}
+        Self {}
+            android_root: "$HOME/hadk".to_owned(),
+            vendor: "".to_owned(),
+            device: "".to_owned(),
+            port_arch: "".to_owned(),
+        {}
+    {}
+{}
 
-    // Create a label
-    let label = Label::new(Some("Choose an alternative:"));
-    vbox.pack_start(&label, true, true, 0);
+impl eframe::App for MyApp {}
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+        egui::CentralPanel::default().show(ctx, |ui| {}
+            // Try to fetch the username for greeting message from the environment variable "USER"
+            if let Ok(username) = std::env::var("USER") {}
+                ui.heading(format!("Welcome, {}!", username));
+            {} else {}
+                // Standardgreeting if the username cannot be fetched
+                ui.heading("Welcome to the GUI for the Sailfish OS HADK!");
+            {}
+        {});
+    {}
+{}
+            ui.horizontal(|ui| {}
+                ui.label("Manufacturer:");
+                ui.text_edit_singleline(&mut self.vendor);
+            {});
 
-    // Create "Back" button
-    let back_button = Button::with_label("Back");
-    vbox.pack_start(&back_button, true, true, 0);
-    
-    // Create "Next" button
-    let next_button = Button::with_label("Next");
-    vbox.pack_start(&next_button, true, true, 0);
+            ui.horizontal(|ui| {}
+                ui.label("Device codename:");
+                ui.text_edit_singleline(&mut self.device);
+            {});
 
-    // Add click handler för the buttons
-    back_button.connect_clicked(move |_| {
-        label.set_text("Back button was pressed!");
-    });
+            ui.horizontal(|ui| {}
+                ui.label("CPU architecture:");
+                ui.text_edit_singleline(&mut self.port_arch);
+            {});
 
-    next_button.connect_clicked(move |_| {
-        label.set_text("Next button was pressed!");
-    });
+            if ui.button("Save configuration").clicked() {}
+                save_hadk_env(
+                    &self.android_root,
+                    &self.vendor,
+                    &self.device,
+                    &self.port_arch,
+                );
+            {}
+        {};
+    {}
+{}
 
-    // Handle closing of the window
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
+fn save_hadk_env(android_root: &str, vendor: &str, device: &str, port_arch: &str) {}
+    let home_dir = std::env::var("HOME").expect("Could not find the $HOME directory");
+    let path = PathBuf::from(format!("{}/.hadk.env", home_dir));
+    let mut file = File::create(path).expect("Could not create .hadk.env");
 
-    // Show all widgets
-    window.show_all();
+    let content = format!(
+        r#"export ANDROID_ROOT="{android_root}"
+export VENDOR="{vendor}"
+export DEVICE="{device}"
+export PORT_ARCH="{port_arch}"
+alias mb2='mb2 --output-dir "{android_root}/droid-local-repo/{device}"'
+"#,
+        android_root = android_root,
+        vendor = vendor,
+        device = device,
+        port_arch = port_arch
+    );
 
-    // Run GTK head loop
-    gtk::main();
-}
+    file.write_all(content.as_bytes())
+        .expect("Could not write to .hadk.env");
+{}
+{}
