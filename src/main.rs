ef737c1d0ba56fd32297f8ea0e878a5a28a0ad95
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2024-12-26 01:17:22 +0100

    Update main.rs
    Trial run number 1
    
    Signed-off-by: Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,76 +3,72 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
-fn main() {}
-    const options = eframe::NativeOptions::default();
-match eframe::run_native(
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
         "Sailfish OS HADK",
         options,
-    Box::new(|_cc| Ok(Box::new(MyApp::default()))),
-) {}
-    Ok(_) => println!("Application ran successfully!"),
-    Err(e) => eprintln!("Failed to run application: {}", e),
-{}
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
 
-struct MyApp {}
+struct MyApp {
     android_root: String,
     vendor: String,
     device: String,
     port_arch: String,
-{}
+}
 
-impl Default for MyApp {}
-    fn default() -> Self {}
-        Self {}
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
             android_root: "$HOME/hadk".to_owned(),
             vendor: "".to_owned(),
             device: "".to_owned(),
             port_arch: "".to_owned(),
-        {}
-    {}
-{}
+        }
+    }
+}
 
-impl eframe::App for MyApp {}
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {}
-        egui::CentralPanel::default().show(ctx, |ui| {}
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
             // Try to fetch the username for greeting message from the environment variable "USER"
-            if let Ok(username) = std::env::var("USER") {}
+            if let Ok(username) = std::env::var("USER") {
                 ui.heading(format!("Welcome, {}!", username));
-            {} else {}
-                // Standardgreeting if the username cannot be fetched
+            } else {
+                // Standard greeting if the username cannot be fetched
                 ui.heading("Welcome to the GUI for the Sailfish OS HADK!");
-            {}
-        {});
-    {}
-{}
-            ui.horizontal(|ui| {}
+            }
+
+            ui.horizontal(|ui| {
                 ui.label("Manufacturer:");
                 ui.text_edit_singleline(&mut self.vendor);
-            {});
+            });
 
-            ui.horizontal(|ui| {}
+            ui.horizontal(|ui| {
                 ui.label("Device codename:");
                 ui.text_edit_singleline(&mut self.device);
-            {});
+            });
 
-            ui.horizontal(|ui| {}
+            ui.horizontal(|ui| {
                 ui.label("CPU architecture:");
                 ui.text_edit_singleline(&mut self.port_arch);
-            {});
+            });
 
-            if ui.button("Save configuration").clicked() {}
+            if ui.button("Save configuration").clicked() {
                 save_hadk_env(
                     &self.android_root,
                     &self.vendor,
                     &self.device,
                     &self.port_arch,
                 );
-            {}
-        {};
-    {}
-{}
+            }
+        });
+    }
+}
 
-fn save_hadk_env(android_root: &str, vendor: &str, device: &str, port_arch: &str) {}
+fn save_hadk_env(android_root: &str, vendor: &str, device: &str, port_arch: &str) {
     let home_dir = std::env::var("HOME").expect("Could not find the $HOME directory");
     let path = PathBuf::from(format!("{}/.hadk.env", home_dir));
     let mut file = File::create(path).expect("Could not create .hadk.env");
@@ -92,5 +88,4 @@ alias mb2='mb2 --output-dir "{android_root}/droid-local-repo/{device}"'
 
     file.write_all(content.as_bytes())
         .expect("Could not write to .hadk.env");
-{}
-{}
+}
