f400e25add5ac3ce9d5e2d933da7dc2e4196e261
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2025-01-10 14:15:20 +0100

    Update main.rs
    Signed-off-by: Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::io;
 
 fn main() {
-    println!("Choose your level of understanding computers:");
+    println!("Choose your level of computer skills:");
 
     let mut val: String = String::new();
 
     println!("1. Beginner");
-    println!("2. Something higher that Beginner");
-    println!("3. Professional hacker");
+    println!("2. Somewhere in between");
+    println!("3. Advanced");
 
     io::stdin().read_line(&mut val).expect("Failed to read input");
 
-    let val: u32 = val.trim().parse().expect("Please choose a number");
+    let val: u32 = val.trim().parse().expect("Please enter a number");
 
     match val {
         1 => println!("You chose Beginner"),
-        2 => println!("You chose Something higher than Beginner"),
-        3 => println!("You chose Professional hacker"),
-        _ => println!("Error! Wrong choise! Please choose a valid option"),
+        2 => println!("You chose Somewhere in between"),
+        3 => println!("You chose Advanced"),
+        _ => println!("Invalid choice! Please select a valid option"),
     }
 }
